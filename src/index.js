83b1b8914fbab0360dfdc2e14ded872002916a68
Lucas <lucasfpd.santos@gmail.com>
2022-03-19 20:42:30 -0300

    add info port

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(rotas);
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
